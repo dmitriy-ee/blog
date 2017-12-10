91e06392cc506c5f92df9146b4e88e02bd3b72a8
dmitriy_ee <dmitriy_ee@hotmail.com>
2017-12-10 21:18:22 +0200

    init database

--- app.rb
+++ app.rb
@@ -1,12 +1,29 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'blog.db'
+  @db.results_as_hash = true
+end
+
+before do
+  init_db
+end
+
+configure do
+
+end
 
 get '/new' do
   erb :new
 end
 
-
+post '/new' do
+  content = params[:content]
+  erb "You typed: #{content}"
+end
 
 
 
