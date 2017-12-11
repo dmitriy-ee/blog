52ae6162d2aea6bb4d869ea4bc00fb395b2c61ee
dmitriy_ee <dmitriy_ee@hotmail.com>
2017-12-11 22:26:24 +0200

    create table comments

--- app.rb
+++ app.rb
@@ -28,6 +28,15 @@ configure do
              created_date DATE, 
              content TEXT
              )'
+
+  # создает таблицу, если она не существует
+  @db.execute 'CREATE TABLE IF NOT EXISTS Comments 
+             (
+             id INTEGER PRIMARY KEY AUTOINCREMENT, 
+             created_date DATE, 
+             content TEXT
+             post_id INTEGER
+             )'
 end
 
 get '/new' do
